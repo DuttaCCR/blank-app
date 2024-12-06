24cab6a47276fb56a2cdbf3ebfae464cc64cf785
DuttaCCR <GDutta@ccrsurveys.com>
2024-12-06 10:37:08 -0600

    Added packages.txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import os
